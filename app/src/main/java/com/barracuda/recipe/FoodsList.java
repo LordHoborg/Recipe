d438d18498791b9d26dfa8c70534841037ef7940
Ali Rezaei <rezaei_ali@rocketmail.com>
2016-07-24 22:57:14 +0430

    اولین ارسال

---
+++ app/src/main/java/com/barracuda/recipe/FoodsList.java
@@ -0,0 +1,15 @@
+package com.barracuda.recipe;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FoodsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.foods_list);
+
+
+    }
+}
---
+++ app/src/main/java/com/barracuda/recipe/MainActivity.java
@@ -0,0 +1,28 @@
+package com.barracuda.recipe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View foodsListButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        foodsListButton = findViewById(R.id.foods_list);
+        foodsListButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == foodsListButton)
+            startActivity(new Intent(this, FoodsList.class));
+
+    }
+}
